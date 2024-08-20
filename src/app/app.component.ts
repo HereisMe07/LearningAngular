ebf6dfb1632039e45eff5920a6556d050e139394
IT_TEK_VITU\Nssf_User <tujames77@gmail.com>
2024-08-20 15:57:05 +0700

    v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgTemplateOutlet } from '@angular/common';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule, NgIf,
-            NgTemplateOutlet, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault],
+            NgTemplateOutlet, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, PostsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -95,6 +96,12 @@ export class AppComponent {
   // }
 
   usersRole: string = 'Editor';
+
+  appPostTitle: string = 'post 1';
+  
+  appIsLogin: boolean = false;
+
+
   }
 
   
---
+++ src/app/posts-list/posts-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './posts-list.component.html',
+  styleUrl: './posts-list.component.css'
+})
+export class PostsListComponent {
+  @Input() postListTitle: string = '';
+  @Input() postIsLogin: boolean = false;
+}
